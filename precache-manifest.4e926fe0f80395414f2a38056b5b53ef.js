3b04718c2610b2c88e92df6bb313185722a48a83
tacoplat <azhen2603@gmail.com>
2022-01-17 20:43:47 -0500

    Updates

---
+++ precache-manifest.4e926fe0f80395414f2a38056b5b53ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8e718cb3f76af60e1bde63f1357f01",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "440b502531a35920aa02",
+    "url": "/robo-friends/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "227595788f697ac48885",
+    "url": "/robo-friends/static/css/main.e9853885.chunk.css"
+  },
+  {
+    "revision": "440b502531a35920aa02",
+    "url": "/robo-friends/static/js/2.0bed7501.chunk.js"
+  },
+  {
+    "revision": "1ce896dddecdb4d51806",
+    "url": "/robo-friends/static/js/3.c541d426.chunk.js"
+  },
+  {
+    "revision": "227595788f697ac48885",
+    "url": "/robo-friends/static/js/main.70217ae3.chunk.js"
+  },
+  {
+    "revision": "b2bd12772fcd76629e17",
+    "url": "/robo-friends/static/js/runtime~main.f428a9bc.js"
+  }
+]);
